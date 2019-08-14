ddfe7bf1080979043ad29fa062b37900c54f67f3
siphesande <sphe.malgas@gmail.com>
2019-08-14 23:54:43 +0200

    Updates

---
+++ precache-manifest.18d963071fb4b3780ce067117862e160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3533e00d22fd5cdea8c2393d0b532d",
+    "url": "/namchef-app/index.html"
+  },
+  {
+    "revision": "b3c6563d8b023c8d0798",
+    "url": "/namchef-app/static/css/main.35424518.chunk.css"
+  },
+  {
+    "revision": "b0dfe611382a852623cd",
+    "url": "/namchef-app/static/js/2.b4cc290c.chunk.js"
+  },
+  {
+    "revision": "b3c6563d8b023c8d0798",
+    "url": "/namchef-app/static/js/main.103dd176.chunk.js"
+  },
+  {
+    "revision": "78f8ae75290a961a86b1",
+    "url": "/namchef-app/static/js/runtime~main.b793984c.js"
+  }
+]);
